52b1fc5989861393e57c9ebf36e09962305debd6
vansh <vanshagarwal11@gmail.com>
2023-07-01 06:39:43 +0530

    Adds partner section

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const landingLeftImg = document.querySelector("#land-left");
 const landingRightImg = document.querySelector("#land-right");
 const ethLeft = document.querySelector("#eth-left");
 const ethRight = document.querySelector("#eth-right");
+const tape1 = document.querySelector("#tape1");
+const tape2 = document.querySelector("#tape2");
+
 
 window.addEventListener("scroll", (e)=>{
   landingLeftImg.style.left = -1 *window.scrollY*25/window.innerHeight +'vw';
   landingRightImg.style.right = -1 *window.scrollY*17/window.innerHeight +'vw';
+  tape1.style.marginLeft = -1*(window.scrollY - 2*window.innerHeight)*50/window.innerHeight + "vw";
+  tape2.style.marginLeft = 1*(window.scrollY - 2*window.innerHeight)*50/window.innerHeight + "vw";
 })
 
 
