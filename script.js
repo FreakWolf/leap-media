54c7ac86435ccb608f9c22f1ded5d9785ce3b102
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-07-05 16:30:30 +0530

    Adds services and contact section

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ window.addEventListener("scroll", (e)=>{
   tape1.style.marginLeft = -1*(window.scrollY - 2*window.innerHeight)*50/window.innerHeight + "vw";
   tape2.style.marginLeft = 1*(window.scrollY - 2*window.innerHeight)*50/window.innerHeight + "vw";
 })
+
+var servicesSection = document.querySelector('.services-sec');
+var servicesImgContainer = document.querySelector('.services-img-container');
+var frameInsideImg = document.querySelector('#frame_inside_img');
+var sectionHeight = servicesSection.offsetHeight;
+var windowHeight = window.innerHeight;
+
+window.addEventListener('scroll', function() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var sectionOffset = servicesSection.offsetTop;
+
+  if (scrollTop >= sectionOffset && scrollTop <= sectionOffset + sectionHeight - windowHeight) {
+    servicesImgContainer.style.position = 'fixed';
+    servicesImgContainer.style.top = '0';
+  }
+  else if (scrollTop >= sectionOffset + sectionHeight - windowHeight) {
+    servicesImgContainer.style.position = 'absolute';
+    servicesImgContainer.style.top = 'auto';
+    servicesImgContainer.style.bottom = '0';
+  }
+  else {
+    servicesImgContainer.style.position = 'absolute';
+    servicesImgContainer.style.top = '0';
+    servicesImgContainer.style.bottom = 'auto';
+  }
+
+  if(scrollTop <= sectionOffset + windowHeight/2){
+    frameInsideImg.src = "./static/video_ads_img.svg";
+    frameInsideImg.alt = "video_ads_img";
+  }
+  else if(scrollTop > sectionOffset + windowHeight/2 && scrollTop <= sectionOffset + (3/2)*windowHeight){
+    frameInsideImg.src = "./static/event_video_production_img.svg";
+    frameInsideImg.alt = "event_video_production_img";
+  }
+  else{
+    frameInsideImg.src = "./static/host_img.svg";
+    frameInsideImg.alt = "host_img";
+  }
+});
