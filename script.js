f812faecf82dfb176a5591cf9fff79ad276afa4a
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2023-07-13 17:33:46 +0530

    Adds pre-loader, decreases load-time and adds some effects

--- script.js
+++ script.js
@@ -16,8 +16,14 @@ if (preloader) {
 window.addEventListener("scroll", (e)=>{
   // landingLeftImg.style.left = -1 *window.scrollY*25/window.innerHeight +'vw';
   // landingRightImg.style.right = -1 *window.scrollY*17/window.innerHeight +'vw';
-  tape1.style.marginLeft = -1*(window.scrollY - window.innerHeight)*50/window.innerHeight + "vw";
-  tape2.style.marginLeft = 1*(window.scrollY - window.innerHeight)*50/window.innerHeight + "vw";
+  if(window.innerWidth < 750){
+  tape1.style.left = -1*(window.scrollY - window.innerHeight)*125/window.innerHeight + "vw";
+  tape2.style.right = -1*(window.scrollY - 1.25*window.innerHeight)*125/window.innerHeight + "vw";
+  }
+  else{
+    tape1.style.left = -1*(window.scrollY - window.innerHeight)*50/window.innerHeight + "vw";
+    tape2.style.right = -1*(window.scrollY - 1.25*window.innerHeight)*50/window.innerHeight + "vw";
+  }
 })
 
 var servicesSection = document.querySelector('.services-sec');
@@ -61,27 +67,69 @@ window.addEventListener('scroll', function() {
   }
 
   if(scrollTop <= sectionOffset + windowHeight/2){
-    frameInsideImg.src = "./static/product_demo_img.svg";
+    frameInsideImg.src = "./static/product_demo_img.png";
     frameInsideImg.alt = "product_demo_img";
   }
   else if(scrollTop > sectionOffset + windowHeight/2 && scrollTop <= sectionOffset + (3/2)*windowHeight){
-    frameInsideImg.src = "./static/social_media_videos_img.svg";
+    frameInsideImg.src = "./static/social_media_videos_img.png";
     frameInsideImg.alt = "social_media_videos_img";
   }
   else if(scrollTop > sectionOffset + (3/2)*windowHeight && scrollTop <= sectionOffset + (5/2)*windowHeight){
-    frameInsideImg.src = "./static/explainer_videos_img.svg";
+    frameInsideImg.src = "./static/explainer_videos_img.png";
     frameInsideImg.alt = "explainer_videos_img";
   }
   else if(scrollTop > sectionOffset + (5/2)*windowHeight && scrollTop <= sectionOffset + (7/2)*windowHeight){
-    frameInsideImg.src = "./static/host_img.svg";
+    frameInsideImg.src = "./static/host_img.png";
     frameInsideImg.alt = "host_img";
   }
   else if(scrollTop > sectionOffset + (7/2)*windowHeight && scrollTop <= sectionOffset + (9/2)*windowHeight){
-    frameInsideImg.src = "./static/event_video_production_img.svg";
+    frameInsideImg.src = "./static/event_video_production_img.png";
     frameInsideImg.alt = "event_video_production_img";
   }
   else{
-    frameInsideImg.src = "./static/social_media_management_img.svg";
+    frameInsideImg.src = "./static/social_media_management_img.png";
     frameInsideImg.alt = "social_media_management_img";
   }
+});
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["users", "eyeballs", "revenue", "brand building"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
