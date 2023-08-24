b86f2a8968ed1966d997bd762127f2b610ac6c43
FreakWolf <rohitskt115@gmail.com>
2023-08-24 18:34:50 +0530

    pre-loader removed

--- script.js
+++ script.js
@@ -1,53 +1,33 @@
 const tape1 = document.querySelector("#tape1");
 const tape2 = document.querySelector("#tape2");
-
-// Preloader
 let preloader = document.querySelector('#preloader');
+
 if (preloader) {
   window.addEventListener('load', () => {
-    setTimeout(() => {
-      preloader.remove();
-    }, 500);
+    setTimeout(() => { preloader.remove(); }, 2500);
   });
 }
 
-// window.addEventListener("scroll", (e)=>{
-//   var windowTop = $(window).scrollTop();
-//   var tape1Top = $("#tape1").offset().top;
-//   var tape2Top = $("#tape2").offset().top;
-//   if(window.innerWidth < 750){
-//     var left1Position = windowTop - tape1Top + window.innerHeight/2;
-//   }else{
-//     var left1Position = windowTop - tape1Top + window.innerHeight/3;
-//   }
-//   var left2Position = windowTop - tape2Top + window.innerHeight/3;
-//   $("#tape1").css({ left: -left1Position });
-//   $("#tape2").css({ left: left2Position });
-// })
-
 var servicesSection = document.querySelector('.services-sec');
 var contactSection = document.querySelector('.contact-sec');
-var servicesImgContainer = document.querySelector('.services-img-container');
-var frameInsideImg = document.querySelector('#frame_inside_img');
 var sectionHeight = servicesSection.offsetHeight;
 var windowHeight = window.innerHeight;
 
 window.addEventListener('scroll', function () {
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  var sectionOffset = servicesSection.offsetTop;
   var contactSecOffset = contactSection.offsetTop;
 
   if (scrollTop <= (2 / 10) * windowHeight) {
-    document.querySelector('.floating-telegram-icon').style.display = 'none';
-    document.querySelector('.floating-meet-btn').style.display = 'none';
+    setDisplay('.floating-telegram-icon', 'none');
+    setDisplay('.floating-meet-btn', 'none');
   }
   else if (scrollTop >= contactSecOffset - windowHeight) {
-    document.querySelector('.floating-telegram-icon').style.display = 'block';
-    document.querySelector('.floating-meet-btn').style.display = 'none';
+    setDisplay('.floating-telegram-icon', 'block');
+    setDisplay('.floating-meet-btn', 'none');
   }
   else {
-    document.querySelector('.floating-telegram-icon').style.display = 'block';
-    document.querySelector('.floating-meet-btn').style.display = 'block';
+    setDisplay('.floating-telegram-icon', 'block');
+    setDisplay('.floating-meet-btn', 'block');
   }
 });
 
@@ -74,4 +54,8 @@ window.onscroll = () => {
       textRevealDiv.classList.remove('show-animate');
     }
   })
+}
+
+function setDisplay(selector, value) {
+  document.querySelector(selector).style.display = value;
 }
